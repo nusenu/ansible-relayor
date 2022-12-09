eb7c743d5fa444354dfaf3a47c129940a91aedfb
nusenu <nusenu-github@riseup.net>
2022-12-09 23:43:29 +0000

    kitchen: remove temporary workaround for hashicorp/vagrant/pull/12584

--- test/vagrant_provisions/openbsd_vagrant_provision.rb
+++ test/vagrant_provisions/openbsd_vagrant_provision.rb
@@ -2,8 +2,5 @@ Vagrant.configure("2") do |config|
   config.vm.provision "shell", inline: <<-SHELL
      sudo pkg_add python3
      sudo ln -s /usr/local/bin/python3 /usr/bin/python
-     # temporary workaround for hashicorp/vagrant/pull/12584
-     sudo echo PubkeyAcceptedAlgorithms +ssh-rsa >> /etc/ssh/sshd_config
-     sudo rcctl restart sshd
   SHELL
 end
--- test/vagrant_provisions/vagrant_provision_ssh_PubkeyAcceptedAlgorithms.rb
+++
@@ -1,6 +0,0 @@
-Vagrant.configure("2") do |config|
-  config.vm.provision "shell", inline: <<-SHELL
-     sudo echo PubkeyAcceptedAlgorithms +ssh-rsa >> /etc/ssh/sshd_config
-     sudo sudo systemctl restart sshd
-  SHELL
-end
