576998e7b71c87821099d8e877534a5faafb9096
nusenu <nusenu-github@riseup.net>
2025-02-15 19:49:21 +0000

    kitchen: overwrite /etc/pkg/FreeBSD.conf to use https mirror

--- test/vagrant_provisions/freebsd_vagrant_provision.rb
+++ test/vagrant_provisions/freebsd_vagrant_provision.rb
@@ -1,5 +1,6 @@
 Vagrant.configure("2") do |config|
   config.vm.provision "shell", inline: <<-SHELL
+     echo 'FreeBSD: { url: "https://pkg.FreeBSD.org/${ABI}/quarterly" , signature_type: "fingerprints", fingerprints: "/usr/share/keys/pkg", enabled: yes }' > /etc/pkg/FreeBSD.conf
      sudo pkg install -y python
   SHELL
 end
