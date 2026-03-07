b6490063a42f52f1b3f0d5ee5689d66f74b67f5c
nusenu <nusenu-github@riseup.net>
2026-03-07 11:31:06 +0000

    kitchen: FreeBSD: use latest repo

--- test/vagrant_provisions/freebsd_vagrant_provision.rb
+++ test/vagrant_provisions/freebsd_vagrant_provision.rb
@@ -1,6 +1,6 @@
 Vagrant.configure("2") do |config|
   config.vm.provision "shell", inline: <<-SHELL
-     echo 'FreeBSD: { url: "https://pkg.FreeBSD.org/${ABI}/quarterly" , signature_type: "fingerprints", fingerprints: "/usr/share/keys/pkg", enabled: yes }' > /etc/pkg/FreeBSD.conf
+     echo 'FreeBSD: { url: "https://pkg.FreeBSD.org/${ABI}/latest" , signature_type: "fingerprints", fingerprints: "/usr/share/keys/pkg", enabled: yes }' > /etc/pkg/FreeBSD.conf
      sudo pkg install -y python
   SHELL
 end
